d94d47a8c48f353a42e10191c99a26c50e41dd38
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-01-30 22:58:52 -0300

    Configurado Container da Lista de Contatos

--- src/containers/ContactList/index.tsx
+++ src/containers/ContactList/index.tsx
@@ -1,13 +1,9 @@
-import { ContactsContainer } from './styles';
+import { ContactsContainer, ListOfContacts } from './styles';
 
 const ContactList = () => (
   <ContactsContainer>
-    <h1>Lista de Contatos</h1>
-    <ul>
-      <li>Contato 1</li>
-      <li>Contato 2</li>
-      <li>Contato 3</li>
-    </ul>
+    <ListOfContacts>
+    </ListOfContacts>
   </ContactsContainer>
 );
 
--- src/containers/ContactList/styles.ts
+++ src/containers/ContactList/styles.ts
@@ -9,3 +9,8 @@ export const ContactsContainer = styled.div`
   border-radius: 8px;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
 `;
+
+export const ListOfContacts = styled.ul`
+  list-style: none;
+  justify-items: start;
+`;
